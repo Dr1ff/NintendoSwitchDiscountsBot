bef8bda4992ebf98db54b5258947f1d91c43e4f6
Dr1ff <dr1ff@yandex.ru>
2022-11-22 15:52:56 +0300

    first commit

---
+++ src/main/java/com/example/nintendoswitchdiscountsbot/NintendoSwitchDiscountsBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.nintendoswitchdiscountsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NintendoSwitchDiscountsBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NintendoSwitchDiscountsBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nintendoswitchdiscountsbot/NintendoSwitchDiscountsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nintendoswitchdiscountsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NintendoSwitchDiscountsBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
