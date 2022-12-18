913cf895cbe2c3984579d18746b881a2eb08c54f
alex-ppv <alex-ppv@yandex-team.ru>
2022-12-18 17:31:55 +0400

    added Country enum

---
+++ src/main/java/com/example/nintendoswitchdiscountsbot/enums/Country.java
@@ -0,0 +1,48 @@
+package com.example.nintendoswitchdiscountsbot.enums;
+
+/**
+ * @author Alexander Popov
+ */
+public enum Country {
+    AT("EUR", "€"),
+    AU("AUD", "$"),
+    BE("EUR", "€"),
+    BG("EUR", "€"),
+    BR("BRL", "R$"),
+    CA("CAD", "$"),
+    CH("CHF", "CHF"),
+    CY("EUR", "€"),
+    CZ("CZK", "Kč"),
+    DE("EUR", "€"),
+    DK("DKK", "Kr"),
+    EE("EUR", "€"),
+    ES("EUR", "€"),
+    FI("EUR", "€"),
+    FR("EUR", "€"),
+    GB("GBP", "£"),
+    GR("EUR", "€"),
+    HR("EUR", "€"),
+    HU("EUR", "€"),
+    IE("EUR", "€"),
+    IT("EUR", "€"),
+    LT("EUR", "€"),
+    LU("EUR", "€"),
+    LV("EUR", "€"),
+    MT("EUR", "€"),
+    MX("MXN", "$"),
+    NL("EUR", "€"),
+    NO("NOK", "kr"),
+    NZ("NZD", "$"),
+    PL("PLN", "zl"),
+    PT("EUR", "€"),
+    RO("EUR", "€"),
+    RU("RUB", "₽"),
+    SE("SEK", "Kr"),
+    SI("EUR", "€"),
+    SK("EUR", "€"),
+    US("USD", "$"),
+    ZA("ZAR", "R");
+
+    Country(String currency, String sign) {
+    }
+}
