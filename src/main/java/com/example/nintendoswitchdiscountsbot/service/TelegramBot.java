4147623d082ea9ebc6f954ac3dd7d6d90ef73977
Dr1ff <dr1ff@yandex.ru>
2022-12-21 15:01:58 +0400

    Добавил UpdateHandler, CommandProcessor, Паттерн Observer KeyboardCallbackProcessor

--- src/main/java/com/example/nintendoswitchdiscountsbot/service/TelegramBot.java
+++ src/main/java/com/example/nintendoswitchdiscountsbot/service/TelegramBot.java
@@ -2,10 +2,9 @@ package com.example.nintendoswitchdiscountsbot.service;
 
 
 import com.example.nintendoswitchdiscountsbot.config.BotConfig;
-import com.example.nintendoswitchdiscountsbot.parser.Parser;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -16,7 +15,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class TelegramBot extends TelegramLongPollingBot {
 
     private final BotConfig config;
-    private final Parser parser;
+    private final UpdateHandler processor;
 
     @Override
     public String getBotUsername() {
@@ -29,8 +28,9 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     @Override
+    @SneakyThrows
     public void onUpdateReceived(Update update) {
-        parser.parse();
+        processor.processing(update);
     }
 }
 
