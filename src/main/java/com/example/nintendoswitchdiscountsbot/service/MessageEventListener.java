435061a2833d0f756aa402a0851c45af9e70524b
Dr1ff <dr1ff@yandex.ru>
2022-12-21 15:01:58 +0400

    Добавил UpdateHandler, CommandProcessor, Паттерн Observer KeyboardCallbackProcessor

---
+++ src/main/java/com/example/nintendoswitchdiscountsbot/service/MessageEventListener.java
@@ -0,0 +1,30 @@
+package com.example.nintendoswitchdiscountsbot.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+
+@Component
+@RequiredArgsConstructor
+public class MessageEventListener {
+
+    private final MessageSender messageSender;
+
+    @EventListener
+    public void handle(SendMessage sendMessage) {
+        messageSender.send(sendMessage);
+    }
+
+    @EventListener
+    public void handle(EditMessageReplyMarkup editMessageReplyMarkup) {
+        messageSender.send(editMessageReplyMarkup);
+    }
+
+    @EventListener
+    public void handle(EditMessageText messageText) {
+        messageSender.send(messageText);
+    }
+}
---
+++ src/main/java/com/example/nintendoswitchdiscountsbot/service/command/StartCommandProcessor.java
@@ -0,0 +1,54 @@
+package com.example.nintendoswitchdiscountsbot.service.command;
+
+import com.example.nintendoswitchdiscountsbot.enums.Command;
+import com.example.nintendoswitchdiscountsbot.service.MessageEventPublisher;
+import com.example.nintendoswitchdiscountsbot.service.command.keyboard.KeyboardService;
+import com.vdurmont.emoji.EmojiParser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+@RequiredArgsConstructor
+public class StartCommandProcessor implements CommandProcessor {
+    private final KeyboardService keyboardService;
+    private final MessageEventPublisher messageEventPublisher;
+    @Override
+    public void process(Update update) {
+//        var user = getUser(update.getMessage().getFrom().getId());
+        SendMessage message = new SendMessage();
+        message.setText(getMessageText(update.getMessage().getFrom().getFirstName()));
+        message.setReplyMarkup(keyboardService.getKeyboard("1"));
+        message.setChatId(update.getMessage().getChatId());
+        messageEventPublisher.publish(message);
+    }
+
+    @Override
+    public Command getCommand() {
+        return Command.START;
+    }
+
+//    private User getUser(Long userId) {
+//        var userO = userStorageService.findById(userId);
+//        User user;
+//        if (userO.isEmpty()) {
+//            user = new User(userId);
+//            userStorageService.add(user);
+//        } else {
+//            user = userO.get();
+//        }
+//        return user;
+//    }
+
+    private String getMessageText(String userFirstName) {
+        return EmojiParser.parseToUnicode("Привет, " +
+                userFirstName +
+                ", я Nintendo Switch Discount Bot " +
+                ":robot_face:" +
+                ":wave:" +
+                "\nДля начала работы выберите ваш регион:");
+    }
+
+
+}
